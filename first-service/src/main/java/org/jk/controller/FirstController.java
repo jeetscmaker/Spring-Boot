185d61f23199c41e18ff7b0eb3d0eea7947e8137
Jitendra <jeetscmaker@gmail.com>
2020-12-24 23:43:17 +0530

    First service.

---
+++ first-service/src/main/java/org/jk/controller/FirstController.java
@@ -0,0 +1,13 @@
+package org.jk.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
+public class FirstController {
+
+	@GetMapping("/message")
+	public String test() {
+		return "Hello World Called in First Service";
+	}
+}
